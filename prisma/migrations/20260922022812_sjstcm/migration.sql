fbb9154f07a6b94b955f58d12211aebeaf72db81
OnyiloBenedictAmeh <153833656+OnyiloBenedictAmeh@users.noreply.github.com>
2026-09-21 19:44:54 -0700

    Update environment variables, enhance seed script, and add favicon links to HTML files

---
+++ prisma/migrations/20260922022812_sjstcm/migration.sql
@@ -0,0 +1,10 @@
+/*
+  Warnings:
+
+  - Added the required column `updatedAt` to the `Class` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "Class" ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "head" TEXT,
+ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL;
